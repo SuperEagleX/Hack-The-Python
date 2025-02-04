dba7495b67efa8d33f687767b9362ef62af1b4cd
SuperEagleX <savakroth17@gmail.com>
2025-02-04 21:22:47 +0700

    Improved Caesar

--- 1-Caesar-Cipher/main.py
+++ 1-Caesar-Cipher/main.py
@@ -30,3 +30,45 @@ message_2 = Caesar_Cipher.Decrypt("gdkkn, h knud bnchmf!", 25)
 
 print(message_1)
 print(message_2)
+
+#Alternative
+
+print("\nImproved Cap Handlings")
+
+class Caesar_2:
+    def __init__(self):
+        self.lowercase = string.ascii_lowercase
+        self.uppercase = string.ascii_uppercase
+
+    def Encrypt(self, text, key):
+        cipher_text = ""
+        for char in text:
+            if char in self.lowercase:
+                new_index = (self.lowercase.index(char) + key) % 26
+                cipher_text += self.lowercase[new_index]
+            elif char in self.uppercase:
+                new_index = (self.uppercase.index(char) + key) % 26
+                cipher_text += self.uppercase[new_index]
+            else:
+                cipher_text += char  # Keep punctuation, spaces, and numbers unchanged
+        return cipher_text
+
+    def Decrypt(self, text, key):
+        plain_text = ""
+        for char in text:
+            if char in self.lowercase:
+                new_index = (self.lowercase.index(char) - key) % 26
+                plain_text += self.lowercase[new_index]
+            elif char in self.uppercase:
+                new_index = (self.uppercase.index(char) - key) % 26
+                plain_text += self.uppercase[new_index]
+            else:
+                plain_text += char
+        return plain_text
+
+Caesar_Cipher_2 = Caesar_2()
+message_3 = Caesar_Cipher_2.Encrypt("Hello, I love Coding!", 25)
+message_4 = Caesar_Cipher_2.Decrypt("gdkkn, h knud bnchmf!", 25)
+
+print(message_3)
+print(message_4)
